cba746659732d5acff95566181eb96f097b5bdea
JV-AZevedo <joaobatata.azevedo.13@gmail.com>
2026-05-19 21:54:44 -0400

    caracteres especiais

---
+++ back-end/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import mysql.connector
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def conectar():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="joao0708",
+        database="usuarios"
+    )
+class UserSenha(BaseModel):
+    usuario: str
+    newusuario: str
+    senha: str
+    confsenha: str
