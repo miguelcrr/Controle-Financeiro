94add890fcc7d34d141e2215165eb19350d4347b
JV-AZevedo <joaobatata.azevedo.13@gmail.com>
2026-06-06 09:14:35 -0400

    falta encaixar token

--- back-end/main.py
+++ back-end/main.py
@@ -18,8 +18,39 @@ def conectar():
         host="localhost",
         user="root",
         password="joao0708",
-        database="usuarios"
+        database="porquinho"
     )
 class UserSenha(BaseModel):
     usuario: str
     senha: str
+
+@app.post("/cadastro")
+def cadastro(user: UserSenha):
+    conexao = conectar()
+    cursor = conexao.cursor()
+    sql = """INSERT INTO usuarios(usuario, senha) VALUES(%s,%s)"""
+    try:
+        cursor.execute(sql,(user.usuario, user.senha))
+        conexao.commit()
+        return {"sucesso": True}
+    except:
+        return {"sucesso": False,"mensagem": "Usuário já existe"}
+    finally:
+            cursor.close()
+            conexao.close()
+
+@app.post("/login")
+def login(user: UserSenha):
+    conexao = conectar()
+    cursor = conexao.cursor(dictionary=True)
+    sql = """SELECT *FROM usuarios WHERE usuario=%s AND senha=%s"""
+    cursor.execute(
+        sql,
+        (user.usuario, user.senha)
+    )
+    resultado = cursor.fetchone()
+    cursor.close()
+    conexao.close()
+    if resultado:
+        return {"sucesso": True}
+    return {"sucesso": False}
--- front-end/script.js
+++ front-end/script.js
@@ -1,51 +1,93 @@
 const API_URL = "http://127.0.0.1:8000";
 
-async function login() {
-  const usuario = document.getElementById("usuario").value;
-  const senha = document.getElementById("senha").value;
-};
-
-
-
-
 function mostrarsenha(){
-
     const senha = document.getElementById("senha");
-    
     if(senha.type === "password"){
         senha.type = "text";
     }
     else{
         senha.type = "password";
     }
-
 }
 
 function mostrarconfsenha(){
-
     const confsenha = document.getElementById("confsenha");
-    
     if(confsenha.type === "password"){
         confsenha.type = "text";
     }
     else{
         confsenha.type = "password";
     }
-
 };
 
-
 const formCadastro = document.getElementById("formCadastro");
-formCadastro.addEventListener("submit", function(event) {
-    const senha = document.getElementById("senha").value;
-    const confsenha = document.getElementById("confsenha");
-    const errosenha = document.getElementById("errosenha");
-    if (senha !== confsenha.value) {
+if (formCadastro) {
+    formCadastro.addEventListener("submit", async function(event) {
         event.preventDefault();
-        errosenha.textContent = "Senhas não coincidem";
-        confsenha.value = "";
-        confsenha.focus();
-    } else {
-        errosenha.textContent = "";
-    }
-});
+        const usuario = document.getElementById("usuario").value;
+        const senha = document.getElementById("senha").value;
+        const confsenha = document.getElementById("confsenha");
+        const errosenha = document.getElementById("errosenha");
+        if (senha !== confsenha.value) {
+            event.preventDefault();
+            errosenha.textContent = "Senhas não coincidem";
+            confsenha.value = "";
+            confsenha.focus();
+        } else {
+            errosenha.textContent = "";
+            const resposta = await fetch("http://127.0.0.1:8000/cadastro",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    usuario,
+                    senha
+                })
+            }
+        );
+        const dados = await resposta.json();
+        if(dados.sucesso){
+            alert("Cadastro realizado!");
+            window.location.href = "Login.html";
+        }
+        else {
+            errosenha.textContent= dados.mensagem;
+        }
+
+        }
+    })
+};
+
+const formLogin = document.getElementById("formLogin");
+if (formLogin) {
+    formLogin.addEventListener("submit", async function(event) {
+        event.preventDefault();
+        const usuario = document.getElementById("usuario").value;
+        const senha = document.getElementById("senha");
+        const erro = document.getElementById("errologin");
+        const resposta = await fetch("http://127.0.0.1:8000/login",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    usuario: usuario,
+                    senha: senha.value
+                })
+            }
+        );
+        const dados = await resposta.json();
+        console.log(dados);
+        if (dados.sucesso) {
+            window.location.href = "index.html";
+        }
+        else {
+            erro.textContent = "Usuário ou senha incorretos";
+            senha.value = "";
+            senha.focus();
+        }
+    })
+};  
