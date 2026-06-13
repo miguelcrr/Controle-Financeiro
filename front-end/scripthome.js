b35fb94a1686cd3af7a6cff1f2ca5e98e1ae1f4a
JV-AZevedo <joaobatata.azevedo.13@gmail.com>
2026-06-13 03:39:25 -0400

    editar+deletar 100% --sem mysql ainda--

--- front-end/scripthome.js
+++ front-end/scripthome.js
@@ -15,42 +15,54 @@ fetch(`http://127.0.0.1:8000/validar?token=${token}`)
 
 let saldo = 0;
 function ganhos() {
-    const janelaGanhos = document.getElementById('janelaGanhos').showModal()
+    document.getElementById('janelaGanhos').showModal();
 }
 function xganhos(){
-    const xganhos = document.getElementById('janelaGanhos').close()
+    document.getElementById('janelaGanhos').close();
 }
-
 function gastos() {
-    const janelaGanhos = document.getElementById('janelaGastos').showModal()
+    document.getElementById('janelaGastos').showModal();
 }
 function xgastos(){
-    const xganhos = document.getElementById('janelaGastos').close()
+    document.getElementById('janelaGastos').close();
+}
+function xedicaoganho(){
+    const erro= document.getElementById("erro3");
+    erro.textContent=""
+    document.getElementById("janelaedicaoganho").close();
+}
+function xedicaogasto(){
+    const erro= document.getElementById("erro4");
+    erro.textContent=""
+    document.getElementById("janelaedicaogastos").close();
 }
 
 function ganhosADC() {
     const valor = document.getElementById("ganho").value;
     const descricao = document.getElementById("descricaoG").value;
-    if (isNaN(valor)) {
-        return;
-    }
-    saldo += parseFloat(valor);
-    attsaldo();
+    const erro = document.getElementById("erro");
+        if(valor === "" ||descricao === "" ||isNaN(valor)){
+            erro.textContent="Preencha todos os campos";
+            return;
+        }
+        else{
+            erro.textContent="";
+        }
+        saldo += parseFloat(valor);
+        attsaldo();
 
     const lista = document.getElementById("listaganhogasto");
-
     const item = document.createElement("div");
-
     item.classList.add("movimentacao", "ganho");
 
     item.innerHTML = `
-        <div>
+        <div class="conteudo">
             ${descricao}
             +R$ ${valor}   
         </div>
         <div class="acoes">
-            <button id="editar">✏️</button>
-            <button id="deletar">🗑️</button>
+            <button id="editar" onclick= "edicaoG(this)">✏️</button>
+            <button id="deletar" onclick="deletarMovimentacao(this)">🗑️</button>
         </div>
     `;
 
@@ -65,26 +77,29 @@ function ganhosADC() {
 function gastosADC() {
     const valor = document.getElementById("gasto").value;
     const descricao = document.getElementById("descricaoP").value;
-    if (isNaN(valor)){
-        return;
-    }
+    const erro2 = document.getElementById("erro2");
+    if(valor === "" ||descricao === "" ||isNaN(valor)){
+            erro2.textContent="Preencha todos os campos";
+            return;
+        }
+        else{
+            erro2.textContent="";
+        }
     saldo-=parseFloat(valor);
     attsaldo();
 
     const lista = document.getElementById("listaganhogasto");
-
     const item = document.createElement("div");
-
     item.classList.add("movimentacao", "gasto");
 
     item.innerHTML = `
-        <div>
+        <div class="conteudo">
             ${descricao}
             -R$ ${valor}
         </div>
         <div class="acoes">
-            <button id="editar">✏️</button>
-            <button id="deletar">🗑️</button>
+            <button id="editar" onclick="edicaoP(this)">✏️</button>
+            <button id="deletar" onclick="deletarMovimentacao(this)">🗑️</button>
         </div>
     `;
 
@@ -99,4 +114,143 @@ function gastosADC() {
 function attsaldo() {
     document.getElementById("saldo").textContent =
         `R$ ${saldo.toFixed(2)}`;
+}
+
+document
+.getElementById("janelaGanhos")
+.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        event.preventDefault();
+        ganhosADC();
+    }
+
+});
+
+document
+.getElementById("janelaGastos")
+.addEventListener("keydown", function(event){
+
+    if(event.key === "Enter"){
+        event.preventDefault();
+        gastosADC();
+    }
+
+});
+
+document
+.getElementById("janelaedicaoganho")
+.addEventListener("keydown", function(event){
+
+    if(event.key === "Enter"){
+
+        event.preventDefault();
+        salvaredicaoG();
+    }
+
+});
+
+document
+.getElementById("janelaedicaogastos")
+.addEventListener("keydown", function(event){
+
+    if(event.key === "Enter"){
+        event.preventDefault();
+        salvaredicaoP();
+    }
+
+});
+
+let itemEditando = null;
+
+function edicaoG(botao){
+
+    itemEditando = botao.closest(".movimentacao");
+
+    const conteudo = itemEditando.querySelector(".conteudo").innerText;
+
+    const partes = conteudo.split("+R$");
+
+    document.getElementById("descricaoeditarG").value =partes[0].trim();
+    document.getElementById("ganhoeditar").value =partes[1].trim();
+    document.getElementById("janelaedicaoganho").showModal();
+}
+
+function salvaredicaoG(){
+    const novoValor = document.getElementById("ganhoeditar").value;
+    const novaDescricao = document.getElementById("descricaoeditarG").value;
+    const erro = document.getElementById("erro3");
+        if(novoValor === "" ||novaDescricao === "" ||isNaN(novoValor)){
+            erro.textContent="Preencha todos os campos";
+            return;
+        }
+        else{
+            erro.textContent="";
+            const valorAntigo = pegarValor(itemEditando);
+            saldo-=parseFloat(valorAntigo)
+            saldo+= parseFloat(novoValor)
+            attsaldo()
+            itemEditando.querySelector(".conteudo").innerHTML =
+        `${novaDescricao} +R$ ${novoValor}`;
+
+        document.getElementById("janelaedicaoganho").close();
+        }
+
+}
+    
+function edicaoP(botao){
+
+    itemEditando =botao.closest(".movimentacao");
+
+    const conteudo =itemEditando.querySelector(".conteudo").innerText;
+    const partes = conteudo.split("-R$");
+    document.getElementById("descricaoeditarP").value =partes[0].trim();
+    document.getElementById("gastoeditar").value =partes[1].trim();
+    document.getElementById("janelaedicaogastos").showModal();
+}
+
+function salvaredicaoP(){
+    const novoValor =document.getElementById("gastoeditar").value;
+    const novaDescricao =document.getElementById("descricaoeditarP").value;
+    const erro = document.getElementById("erro4");
+    if(novoValor === "" ||novaDescricao === "" ||isNaN(novoValor)){
+            erro.textContent="Preencha todos os campos";
+            return;
+        }
+        else{
+            erro.textContent=""
+            const valorAntigo = pegarValor(itemEditando);
+            saldo+=parseFloat(valorAntigo);
+            saldo-=parseFloat(novoValor);
+            attsaldo();
+        itemEditando.querySelector(".conteudo").innerHTML =
+            `${novaDescricao} -R$ ${novoValor}`;
+
+        document.getElementById("janelaedicaogastos").close();
+            }
+}
+
+function pegarValor(item){
+    const texto = item.querySelector(".conteudo").innerText;
+
+    const numero =texto.match(/[+-]?R\$\s*(\d+(\.\d+)?)/);
+
+    return numero
+    ? parseFloat(numero[1]): 0;
+
+}
+
+function deletarMovimentacao(botao){
+
+    const item =botao.closest(".movimentacao");
+
+    const valor =pegarValor(item);
+
+    if(item.classList.contains("ganho")){
+        saldo -= valor;
+    }
+    else{
+        saldo += valor;
+    }
+    attsaldo();
+    item.remove();
 }
